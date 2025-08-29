0bcc50f740b2e2c8bd2a2dcb77e55d3549133831
Ahmed Anwar <ahmedmanwar63@gmail.com>
2025-08-29 19:04:32 +0300

    pushing terraform files

---
+++ public_subnets.tf
@@ -0,0 +1,12 @@
+resource "aws_subnet" "Public_Subnet_zone1" {
+  vpc_id     = aws_vpc.Anwar_VPC_1.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+resource "aws_subnet" "Public_Subnet_zone2" {
+  vpc_id     = aws_vpc.Anwar_VPC_1.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+}
