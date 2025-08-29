773e86ac2522144053af92b2dde7e7dbffecae1b
Ahmed Anwar <ahmedmanwar63@gmail.com>
2025-08-29 20:03:55 +0300

    Remove duplicate files

--- Terraform/IGW.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.Anwar_VPC_1.id
-  tags = {
-      Name = "IGW"
-  }
-}
--- vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "Anwar_VPC_1" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "Anwar VPC Project"
-  }
-}
